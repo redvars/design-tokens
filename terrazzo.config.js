9e1689f44f9be8e6bbed30daf9b9340eefcbc78a
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2025-11-08 16:50:51 +0000

    initial commit

---
+++ terrazzo.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "@terrazzo/cli";
+import pluginCSS from "@terrazzo/plugin-css";
+import pluginJS from "@terrazzo/plugin-js";
+import pluginSASS from "@terrazzo/plugin-sass";
+
+export default defineConfig({
+  tokens: ["./tokens.json"],
+  outDir: "./tokens/",
+
+  plugins: [
+    pluginCSS(), // generate .css files
+    pluginJS({
+      js: "index.js",
+      ts: "index.d.ts",
+      json: "token.json", // set to a filename to generate JSON
+    }),
+    pluginSASS({
+      filename: "index.scss",
+    }),
+  ],
+
+  lint: {
+    rules: {
+      // my lint rules
+    },
+  },
+});
